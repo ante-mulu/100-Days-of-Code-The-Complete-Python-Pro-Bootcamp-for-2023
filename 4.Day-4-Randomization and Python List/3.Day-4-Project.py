6c7be1722e5b99aab9ef52f12b3f7e4c6c8660cc
Anteneh <anteneh.mulu1921@gmail.com>
2023-12-04 16:35:48 +0300

    Day-4 Code and Note

--- 4.Day-4-Randomization and Python List/3.Day-4-Project.py
+++ 4.Day-4-Randomization and Python List/3.Day-4-Project.py
@@ -1,3 +1,4 @@
+import random
 rock = '''
     _______
 ---'   ____)
@@ -26,3 +27,18 @@ scissors = '''
 '''
 
 # Write your code below this line 👇
+user_choice = int(input(
+    "what do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
+RPS = [rock, paper, scissors]
+print(RPS[user_choice])
+random_rps = random.choice(RPS)
+random_rps_index = RPS.index(random_rps)
+print(f"Computer chose:\n")
+print(RPS[random_rps_index])
+
+if ((user_choice == 0 and random_rps_index == 2) or (user_choice == 1 and random_rps_index == 0) or (user_choice == 2 and random_rps_index == 1)):
+    print("You win!")
+elif (user_choice == random_rps_index):
+    print("It's a draw.")
+else:
+    print("You lose")
