79caebe7cbab3fae911d94915e46cfcc2e3bf065
Anteneh <anteneh.mulu1921@gmail.com>
2023-12-10 12:56:25 +0300

    Day-10 Code and Note

---
+++ 10.Functions _with_Outputs/1.function_with_output.py
@@ -0,0 +1,15 @@
+# a function with output means retuning some value from function after the task of that function is finished
+# a function that multiply two number
+# def multiply():
+#     result = 4*5
+#     return result
+
+
+# print(multiply())  # return and  the result which is 20
+
+# Challenge-1:Change the string in to title case
+def format_name(f_name, l_name):
+    return f_name.title() + " " + l_name.title()  # this is multiple return statement
+
+
+print(format_name("abebe atere demelash", "kebede"))
